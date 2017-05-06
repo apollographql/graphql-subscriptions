7f239ca1a0ec4369488d3bdd8bad181a121aca31
Urigo <uri.goldshtein@gmail.com>
2017-05-06 15:14:06 +0300

    feat(new-graphql): Fix for filters logic

--- src/with-filter.ts
+++ src/with-filter.ts
@@ -5,22 +5,26 @@ export type ResolverFn = (rootValue?: any, args?: any, context?: any, info?: any
 
 export const withFilter = (asyncIterator: AsyncIterator<any>, filterFn: FilterFn): Function => {
   return (rootValue: any, args: any, context: any, info: any): AsyncIterator<any> => {
+    const getNextPromise = () => {
+      return asyncIterator
+        .next()
+        .then(payload => Promise.all([
+          payload,
+          Promise.resolve(filterFn(payload.value, args, context, info)).catch(() => false),
+        ]))
+        .then(([payload, filterResult]) => {
+          if (filterResult === true) {
+            return payload;
+          }
+
+          // Skip the current value and wait for the next one
+          return getNextPromise();
+        });
+    };
+
     return {
       next() {
-        return asyncIterator
-          .next()
-          .then(payload => Promise.all([
-            payload,
-            Promise.resolve(filterFn(payload.value, args, context, info)).catch(() => false),
-          ]))
-          .then(([payload, filterResult]) => {
-            if (filterResult === true) {
-              return payload;
-            }
-
-            // Skip the current value and wait for the next one
-            return asyncIterator.next();
-          });
+        return getNextPromise();
       },
       return() {
         return Promise.resolve({ value: undefined, done: true });
