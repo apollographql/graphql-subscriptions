60d79b404b0bf6f0c61e41f7d680beacccea4c9c
Hieu Tran <ht@Hieus-MacBook-Pro.local>
2017-11-21 14:17:14 +0700

    FilterFn return boolean | Promise<boolean>

--- src/with-filter.ts
+++ src/with-filter.ts
@@ -1,6 +1,6 @@
 import { $$asyncIterator } from 'iterall';
 
-export type FilterFn = (rootValue?: any, args?: any, context?: any, info?: any) => boolean;
+export type FilterFn = (rootValue?: any, args?: any, context?: any, info?: any) => boolean | Promise<boolean>;
 export type ResolverFn = (rootValue?: any, args?: any, context?: any, info?: any) => AsyncIterator<any>;
 
 export const withFilter = (asyncIteratorFn: ResolverFn, filterFn: FilterFn): ResolverFn => {
