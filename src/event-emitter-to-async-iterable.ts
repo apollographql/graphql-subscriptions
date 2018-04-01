fd292280f2f91e8cc4adaf373b85bc5391391956
Andy Edwards <jedwards@fastmail.com>
2018-04-01 17:29:24 -0500

    fix: ignore TypeScript error concerning iterall

--- src/event-emitter-to-async-iterable.ts
+++ src/event-emitter-to-async-iterable.ts
@@ -5,6 +5,7 @@ import { eventEmitterAsyncIterator } from './event-emitter-to-async-iterator';
 
 export function eventEmitterAsyncIterable<T>(eventEmitter: EventEmitter,
                                              eventsNames: string | string[]): AsyncIterable<T> {
+  // @ts-ignore: $$asyncIterator is considered the same as Symbol.asyncIterator by TypeScript
   return {
     [$$asyncIterator]() {
       return eventEmitterAsyncIterator(eventEmitter, eventsNames);
