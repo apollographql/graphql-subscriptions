6485f1a68100a3d43ed13f5417d4bfd966c3114a
Andy Edwards <jedwards@fastmail.com>
2018-04-01 17:22:20 -0500

    fix: method name typo

--- src/event-emitter-to-async-iterable.ts
+++ src/event-emitter-to-async-iterable.ts
@@ -1,13 +1,13 @@
 import { $$asyncIterator } from 'iterall';
 import { EventEmitter } from 'events';
 
-import { eventEmitterToAsyncIterator } from './event-emitter-to-async-iterator';
+import { eventEmitterAsyncIterator } from './event-emitter-to-async-iterator';
 
 export function eventEmitterAsyncIterable<T>(eventEmitter: EventEmitter,
                                              eventsNames: string | string[]): AsyncIterator<T> {
   return {
     [$$asyncIterator]() {
-      return eventEmitterToAsyncIterator(eventEmitter, eventsNames);
+      return eventEmitterAsyncIterator(eventEmitter, eventsNames);
     },
   };
 }
