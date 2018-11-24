9d8471f4f4d82f5d167ef315a9297fe44352e411
robbywashere <robertapolana@gmail.com>
2018-11-23 21:28:15 -0500

    Changed async iterator code

--- src/event-emitter-to-async-iterator.ts
+++ src/event-emitter-to-async-iterator.ts
@@ -1,75 +1,16 @@
-import { $$asyncIterator } from 'iterall';
-import { EventEmitter } from 'events';
+import { EventEmitter } from "events";
 
-export function eventEmitterAsyncIterator<T>(eventEmitter: EventEmitter,
-                                             eventsNames: string | string[]): AsyncIterator<T> {
-  const pullQueue = [];
-  const pushQueue = [];
-  const eventsArray = typeof eventsNames === 'string' ? [eventsNames] : eventsNames;
-  let listening = true;
-  let addedListeners = false;
-
-  const pushValue = event => {
-    if (pullQueue.length !== 0) {
-      pullQueue.shift()({ value: event, done: false });
-    } else {
-      pushQueue.push(event);
+export async function* eventEmitterAsyncIterator<T>(
+  emitter: EventEmitter,
+  event: string | string[]
+): AsyncIterator<T> {
+  let q = [];
+  const events = [].concat(event);
+  events.forEach(e => emitter.on(e, arg => q.push(arg)));
+  while (true) {
+    await Promise.race(events.map(e => new Promise(rs => emitter.once(e, rs))));
+    while (q.length) {
+      yield q.shift();
     }
-  };
-
-  const pullValue = () => {
-    return new Promise(resolve => {
-      if (pushQueue.length !== 0) {
-        resolve({ value: pushQueue.shift(), done: false });
-      } else {
-        pullQueue.push(resolve);
-      }
-    });
-  };
-
-  const emptyQueue = () => {
-    if (listening) {
-      listening = false;
-      if (addedListeners) { removeEventListeners(); }
-      pullQueue.forEach(resolve => resolve({ value: undefined, done: true }));
-      pullQueue.length = 0;
-      pushQueue.length = 0;
-    }
-  };
-
-  const addEventListeners = () => {
-    for (const eventName of eventsArray) {
-      eventEmitter.addListener(eventName, pushValue);
-    }
-  };
-
-  const removeEventListeners = () => {
-    for (const eventName of eventsArray) {
-      eventEmitter.removeListener(eventName, pushValue);
-    }
-  };
-
-  return {
-    next() {
-      if (!listening) { return this.return(); }
-      if (!addedListeners) {
-        addEventListeners();
-        addedListeners = true;
-      }
-      return pullValue();
-    },
-    return() {
-      emptyQueue();
-
-      return Promise.resolve({ value: undefined, done: true });
-    },
-    throw(error) {
-      emptyQueue();
-
-      return Promise.reject(error);
-    },
-    [$$asyncIterator]() {
-      return this;
-    },
-  };
+  }
 }
