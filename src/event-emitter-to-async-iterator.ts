272721df3a6b8c7cfafc476c7ab45aba1cbf684a
Robby <robbywashere@users.noreply.github.com>
2018-11-29 18:49:12 -0500

    Update event-emitter-to-async-iterator.ts

--- src/event-emitter-to-async-iterator.ts
+++ src/event-emitter-to-async-iterator.ts
@@ -2,13 +2,13 @@ import { EventEmitter } from 'events';
 
 export async function* eventEmitterAsyncIterator<T>(
   emitter: EventEmitter,
-  event: string | string[]
+  events: string | string[]
 ): AsyncIterator<T> {
   let q = [];
-  const events = [].concat(event);
-  events.forEach(e => emitter.on(e, arg => q.push(arg)));
+  const eventsArray = [].concat(events);
+  for (let event of eventsArray) emitter.on(event,arg => q.push(arg));
   while (true) {
-    await Promise.race(events.map(e => new Promise(rs => emitter.once(e, rs))));
+    await new Promise(rs=>setImmediate(rs));
     while (q.length) {
       yield q.shift();
     }
