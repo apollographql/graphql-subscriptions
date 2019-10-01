f7395a5205a5a2d9aaa6cdbf33300340c2b8f5d6
Jan Halama <halama@jablotron.cz>
2019-10-01 21:16:39 +0200

    fix: add withFilterAsync and ResolverFnAsync to index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { PubSubEngine } from './pubsub-engine';
 export { PubSub, PubSubOptions } from './pubsub';
 export { withFilter, ResolverFn, FilterFn } from './with-filter';
+export { withFilterAsync, ResolverFnAsync } from './with-filter-async';
