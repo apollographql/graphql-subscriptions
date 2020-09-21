2d123cec665dcb001a346626d9991efa8b57d253
Laurin Quast <laurinquast@googlemail.com>
2020-09-21 18:41:39 +0200

    fix(typings): return AsyncIterableIterator instead of AsyncIterator
    BREAKING fixes the type annotation of the abstract class PubSubEngine. According to the TypeScript type-defintion a `PubSubAsyncIterator` instance is actually a `AsyncIterableIterator` instead of an  `AsyncIterator`. The typing of `PubSubAsyncIterator` is way more convenient as it allows iterating over it with the `for await (const foo of iterator) { doSth() }` syntax, which is super handy for filtering or mapping (See https://gist.github.com/n1ru4l/127178705cc0942cad0e45d425e2eb63 for some example operators).

--- src/pubsub-engine.ts
+++ src/pubsub-engine.ts
@@ -4,7 +4,7 @@ export abstract class PubSubEngine {
   public abstract publish(triggerName: string, payload: any): Promise<void>;
   public abstract subscribe(triggerName: string, onMessage: Function, options: Object): Promise<number>;
   public abstract unsubscribe(subId: number);
-  public asyncIterator<T>(triggers: string | string[]): AsyncIterator<T> {
+  public asyncIterator<T>(triggers: string | string[]): AsyncIterableIterator<T> {
     return new PubSubAsyncIterator<T>(this, triggers);
   }
 }
